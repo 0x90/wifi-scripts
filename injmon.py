212a35f63daf9b08bf694f63d986939cfde45534
090h <root@0x90.ru>
2014-10-25 00:48:48 +0400

    + injmon - cleanup

---
+++ injmon.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from sys import argv
+from PyLorcon2 import Context, auto_driver
+
+# import scapy silently
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+# from scapy.all import *
+from scapy.all import get_if_list, conf
+conf.verb = 0
+
+
+
+def injmon(iface=None):
+    if iface is None:
+        for i in get_if_list():
+            injmon(i)
+    else:
+        try:
+            driver, description = auto_driver(iface)
+            print('Enabling injmon on %s %s' % (iface, driver))
+        except:
+            # print('Could not find driver for %s' % iface)
+            return
+        if iface.find('mon') != -1:
+            print('%s is already in mon mode' % iface)
+            return
+
+        ctx = Context(iface)
+        try:
+            ctx.open_injmon()
+            # ctx.s
+        except:
+            print("Failed to set injmon for %s!" % iface)
+
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        injmon()
+    else:
+        injmon(argv[1])
