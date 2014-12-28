6d72e4d2d00c5f6e88c986f7916f321747d3786d
0x90 <oleg.kupreev@gmail.com>
2014-12-28 16:59:10 +0300

    +nolimits.sh

--- nolimits.sh
+++ nolimits.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+card=$1
+
 #1st Method.
 iw reg set BO
-iwconfig wlan0 txpower 30
+iwconfig $card  txpower 30
 
 #2nd Method.
-ifconfig wlan0 down
+ifconfig $card down
 iw reg set BO
-ifconfig wlan0 up
-iwconfig wlan0 channel 13
-iwconfig wlan0 txpower 30
+ifconfig $card up
+iwconfig $card channel 13
+iwconfig $card txpower 30
